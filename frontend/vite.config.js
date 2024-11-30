670ad3a7bf85578ed5ee732f22b21e0527b7d6e3
Suvakshya Acharya <asuvaksha@gmail.com>
2024-11-30 15:23:34 +0545

    tenth commit "sign up functionality completed "

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,20 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000", //thsi proxy code is an alternative for cors for connecting the backend and frontend
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});

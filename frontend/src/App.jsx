281d022e04042c8847dc569e64a0cfaf2914c9fc
Suvakshya Acharya <asuvaksha@gmail.com>
2024-11-30 08:56:58 +0545

    fourth commit "header ui + routing completed"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,28 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import SignInForm from './auth/forms/SignInForm'
-import SignUpForm from './auth/forms/SignUpForm'
-import Home from './pages/Home'
-import About from './pages/About'
-import Dashboard from './pages/Dashboard'
-import NewsArticles from './pages/NewsArticles'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignInForm from "./auth/forms/SignInForm";
+import SignUpForm from "./auth/forms/SignUpForm";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import NewsArticles from "./pages/NewsArticles";
+import Header from "./components/shared/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path="/sign-in" element={<SignInForm/> } />
+        <Route path="/sign-in" element={<SignInForm />} />
         <Route path="/sign-up" element={<SignUpForm />} />
 
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About/>} />
+        <Route path="/about" element={<About />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/news" element={<NewsArticles />} />
-
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
